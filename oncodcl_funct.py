ec6fbabd7598316005cbdc52977c85ae93c24704
carnedo <claudia.arnedo@irbbarcelona.org>
2017-08-03 10:30:41 +0200

    Multiprocessing at gene level added
    oncodcl_funct.py all functions except run and main
    oncodcl_funct2.py run function, calls oncodcl_funct.py
    oncodcl_run.py main function calling oncodcl_funct2.py (multiprocessing), also parsing regions and mutations
    oncodcl_c.ipynb modified
    
    Signed-off-by: carnedo <claudia.arnedo@irbbarcelona.org>

--- oncodcl_funct.py
+++ oncodcl_funct.py
@@ -1,13 +1,11 @@
 # Oncodriveclustl functions
-
 import gzip
 import math as m
 from collections import defaultdict
-
 import numpy as np
 from intervaltree import IntervalTree
 
-# Parse regions
+
 def regions(input_regions):
     """
     Parse input regions
@@ -15,11 +13,9 @@ def regions(input_regions):
     :return:
         trees: dictionary of dictionary of intervaltrees (trees) containing intervals of genomic regions by chromosome.
         regions_d: dictionary containing a list of tuples with the coding sequences of each gene.
-        strands_d: dictionary of genes (keys) and strand (value)
     """
     trees = defaultdict(IntervalTree)
     regions_d = defaultdict(list)
-    strands_d = defaultdict()
 
     with gzip.open(input_regions, 'rb') as fd:  # rb for binary <-- gz
         for line in fd:
@@ -28,10 +24,9 @@ def regions(input_regions):
             if 'ENSGR' not in ensid:
                 trees[chromosome][int(start): int(end) + 1] = symbol  # int, +1 end
                 regions_d[symbol].append((int(start), int(end) + 1))
-                if symbol not in strands_d.keys():
-                    strands_d[symbol] = strand
 
-    return trees, regions_d, strands_d
+    return trees, regions_d
+
 
 # Read mutations file and intersect with regions
 def read_mutations(input_mutations, trees):
@@ -41,7 +36,7 @@ def read_mutations(input_mutations, trees):
     chr \t position \t ref \t alt \t sample \t mut_type \t tumor \t signature \ transcript \ symbol
     :param trees: dictionary of dictionary of intervaltrees containing intervals of genomic regions by chromosome.
     :return:
-        mutations_d: dictionary containing a list of mutations by gene
+        mutations_d: dictionary, key = gene, value = list of mutations per gene
     """
     mutations_d = defaultdict(list)
 
@@ -56,6 +51,7 @@ def read_mutations(input_mutations, trees):
 
     return mutations_d
 
+
 # Smooth
 def tukey(window):
     """Tukey smoothing function
@@ -67,6 +63,7 @@ def tukey(window):
     filter_ = tukey(np.arange(-half_window, half_window + 1) / (half_window + 1))
     filter_ = filter_ / sum(filter_)
     return filter_
+
 
 def smoothing(symbol, regions, mutations, window):
     """
@@ -90,7 +87,7 @@ def smoothing(symbol, regions, mutations, window):
     region_info = defaultdict()
     genomic = np.array([])  # genomic coordinates
 
-    for element in regions[symbol]:  # iterate through tuples of coordinates
+    for element in regions:  # iterate through tuples of coordinates
         for position in range(element[0], element[1] + 1):
             genomic = np.append(genomic, [position])
 
@@ -99,28 +96,26 @@ def smoothing(symbol, regions, mutations, window):
 
     indeces = np.searchsorted(np.array(genomic), mutations)
 
-    try:
-        for index in indeces:
-            binary[index: index + window] += smooth
-            mutations_a[index + window // 2] += 1
-    except ValueError as e:
-        print("Error, gene %s in pseudoautosomal region" %(symbol))
+    for index in indeces:
+        binary[index: index + window] += smooth
+        mutations_a[index + window // 2] += 1
 
     # Remove the extra bases at the beginning and at the end
     binary = binary[window // 2: - window // 2 + 1]
     mutations_a = mutations_a[window // 2: - window // 2 + 1]
-      
+
     region_info['symbol'] = symbol
     region_info['genomic'] = genomic
     region_info['binary'] = binary
     region_info['mutations'] = mutations_a
-              
+
     return region_info
 
-# Cluster
+
+# Clusters
 def find_locals(regions):
     """
-    For a given region, find local maximum and minimum.
+    For a given region, find local maximum and minimum
     :param regions:  dictionary containing information for a gene (symbol, genomic, binary, mutations)
     :return:
         idexes: list of tuples of 3 elements (min or max, cds region, smoothing score). Min == 0, max == 1
@@ -130,10 +125,10 @@ def find_locals(regions):
     maxs = []
        
     # Iterate through the binary array
-    for i in range(len(regions['binary'])): # don't add 1, using indexes ---> range(0,len) == {0, len-1}
+    for i in range(len(regions['binary'])):  # don't add 1, using indexes ---> range(0,len) == {0, len-1}
         
         # for max and min in range(1,len-1)
-        if i !=0 and i != len(regions['binary'])-1: 
+        if i != 0 and i != len(regions['binary'])-1:
             # Max
             # When max score is equal in contiguous positions, assumes the first as max
             if regions['binary'][i] > regions['binary'][i - 1] and regions['binary'][i] >= regions['binary'][i + 1]:
@@ -164,9 +159,10 @@ def find_locals(regions):
                 
     return indexes, maxs
 
+
 def raw_clusters(indexes):
     """
-    Define a cluster per maximum found
+    Define a cluster per maximum found in a region
     :param indexes: list of tuples of 3 elements (min or max, cds region, smoothing score). Min == 0, max == 1
     :return:
         clusters: dictionary of dictionaries of clusters for a gene. Each cluster, named by position from 0 to n of
@@ -187,7 +183,7 @@ def raw_clusters(indexes):
             clusters[j]['min_l'] = [indexes[i - 1][1], indexes[i - 1][2]]
             clusters[j]['min_r'] = [indexes[i + 1][1], indexes[i + 1][2]]
 
-            # if it's the first cluster
+        # if it's the first cluster
         elif i == 0: 
             clusters[j]['min_l'] = []
             clusters[j]['min_r'] = [indexes[i + 1][1], indexes[i + 1][2]]
@@ -201,13 +197,14 @@ def raw_clusters(indexes):
 
     return clusters
 
+
 def merge_clusters(clusters, maxs, window):
     """
     Given a number of clusters in a region, iterate through them to merge them if their maximums are closer than a given
     length.
     :param clusters: dictionary of dictionary of clusters for a gene.
     :param maxs: list of maximum positions in a region
-    :param window: clustering window to be added to cluster right minimum
+    :param window: int, clustering window. It is added to cluster right minimum
     :return:
         clusters: dictionary of dictionaries
     """
@@ -226,7 +223,8 @@ def merge_clusters(clusters, maxs, window):
 
                 # When the intersection between the positions of the search and the maximums is not empty
                 if search_r.intersection(maxs_set) != set():
-                    intersect_max = maxs.index(sorted(list(search_r.intersection(maxs_set)))[0])# Analyze only the closest max
+                    # Analyze only the closest max
+                    intersect_max = maxs.index(sorted(list(search_r.intersection(maxs_set)))[0])
                     stop = 1
 
                     # When the testing max is greater or equal than the intersected max,
@@ -248,173 +246,107 @@ def merge_clusters(clusters, maxs, window):
 
     return clusters
 
-def clusters_mut(clusters, regions, mutations, mut_cutoff):
+
+def clusters_mut(clusters, regions, mutations):
     """
     Calculates the number of mutations within a cluster
     :param clusters: dictionary of dictionary containing clusters
     :param regions: dictionary containing information for a gene (symbol, genomic, binary, mutations)
     :param mutations: list of mutations for a region
-    :param mut_cutoff: int, threshold of mutations within a cluster
     :return:
         clusters: dictionary of dictionaries with new key 'n_mutations' added
     """
     for cluster, values in clusters.items():
-        cluster_muts = []
 
-        if values['min_l'] != [] and values['min_r'] != []:
+        if values['min_l'] and values['min_r']:
             cluster_range = range(int(regions['genomic'][values['min_l'][0]]), int(regions['genomic'][values['min_r'][0]] + 1))
             cluster_muts = [i for i in mutations if i in cluster_range]
             clusters[cluster]['n_mutations'] = len(cluster_muts)
 
-        elif values['min_l'] == []:
+        elif not values['min_l']:
             cluster_range = range(int(regions['genomic'][values['max'][0]]), int(regions['genomic'][values['min_r'][0]] + 1))
             cluster_muts = [i for i in mutations if i in cluster_range]
             clusters[cluster]['n_mutations'] = len(cluster_muts)
 
-        elif values['min_r'] == []:
+        elif not values['min_r']:
             cluster_range = range(int(regions['genomic'][values['min_l'][0]]), int(regions['genomic'][values['min_r'][0]] + 1))
             cluster_muts = [i for i in mutations if i in cluster_range]
             clusters[cluster]['n_mutations'] = len(cluster_muts)
 
-    # Filter clusters, keep > mut_cutoff
-    clusters = {key: value for key, value in clusters.items() if value['n_mutations'] >= mut_cutoff}
-
     return clusters
 
-def score_clusters(clusters, regions, mutations, mode):
+
+def score_clusters(clusters, regions):
     """
     Score clusters
     :param clusters: dictionary of dictionaries of clusters
     :param regions: dictionary containing information for a gene (symbol, genomic, binary, mutations)
-    :param mutations: list of mutations for a region
-    :param mode: str, scoring {'nmutations', 'genelength', 'nobias'}
     :return: dictionary of dictionaries, with key 'score' added
     """
     root = m.sqrt(2)
 
-    if mode == 'nmutations':
-        for cluster, values in clusters.items():
-            score = []
+    for cluster, values in clusters.items():
+        score = []
 
-            if values['min_l'] != [] and values['min_r'] != []:
-                for position in range(values['min_l'][0], values['min_r'][0] + 1):  # include mins
-                    fraction_mut = (regions['mutations'][position] / len(mutations)) * 100
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
+        if values['min_l'] and values['min_r']:
+            for position in range(values['min_l'][0], values['min_r'][0] + 1):  # include mins
+                fraction_mut = regions['mutations'][position]
+                distance = abs(values['max'][0] - position)
+                score.append(fraction_mut / m.pow(root, distance))
 
-            elif values['min_l'] == []:
-                for position in range(values['max'][0] + 1, values['min_r'][0] + 1):
-                    fraction_mut = (regions['mutations'][position] / len(mutations)) * 100
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
+        elif not values['min_l']:
+            for position in range(values['max'][0] + 1, values['min_r'][0] + 1):
+                fraction_mut = regions['mutations'][position]
+                distance = abs(values['max'][0] - position)
+                score.append(fraction_mut / m.pow(root, distance))
 
-            elif values['min_r'] == []:
-                for position in range(values['min_l'][0], values['max'][0]):
-                    fraction_mut = (regions['mutations'][position] / len(mutations)) * 100
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
+        elif not values['min_r']:
+            for position in range(values['min_l'][0], values['max'][0]):
+                fraction_mut = regions['mutations'][position]
+                distance = abs(values['max'][0] - position)
+                score.append(fraction_mut / m.pow(root, distance))
 
-            # Update score
-            clusters[cluster]['score'] = sum(score)
+        # Update score
+        clusters[cluster]['score'] = sum(score)
 
-    elif mode == 'genelength':
-        for cluster, values in clusters.items():
-            score = []
-
-            if values['min_l'] != [] and values['min_r'] != []:
-                for position in range(values['min_l'][0], values['min_r'][0] + 1): # include mins
-                    fraction_mut = (regions['mutations'][position] / len(regions['mutations'])) * 100
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
-
-            elif values['min_l'] == []:
-                for position in range(values['max'][0] + 1, values['min_r'][0] + 1):
-                    fraction_mut = (regions['mutations'][position] / len(regions['mutations'])) * 100
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
-
-            elif values['min_r'] == []:
-                for position in range(values['min_l'][0], values['max'][0]):
-                    fraction_mut = (regions['mutations'][position] / len(regions['mutations'])) * 100
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
-
-            # Update score
-            clusters[cluster]['score'] = sum(score)
-
-    elif mode == 'nobias':
-        for cluster, values in clusters.items():
-            score = []
-
-            if values['min_l'] != [] and values['min_r'] != []:
-                for position in range(values['min_l'][0], values['min_r'][0] + 1):  # include mins
-                    fraction_mut = regions['mutations'][position]
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
-
-            elif values['min_l'] == []:
-                for position in range(values['max'][0] + 1, values['min_r'][0] + 1):
-                    fraction_mut = regions['mutations'][position]
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
-
-            elif values['min_r'] == []:
-                for position in range(values['min_l'][0], values['max'][0]):
-                    fraction_mut = regions['mutations'][position]
-                    distance = abs(values['max'][0] - position)
-                    score.append(fraction_mut / m.pow(root, distance))
-
-            # Update score
-            clusters[cluster]['score'] = sum(score)
-
-    
     return clusters
 
-def clustering(regions, mutations, window, cutoff, mode):
+
+def clustering(regions, mutations, window):
     """
     Given a gene, calculate and score its clusters
     :param regions: dictionary containing information for a gene (symbol, genomic, binary, mutations)
     :param mutations: list of mutations for a region
     :param window: int, clustering window for clusters merging
-    :param cutoff: int, minimum number of mutations for a cluster
-    :param mode: str, clusters score
     :return: dictionary of dictionaries of clusters within a region
     """
     indexes, maxs_list = find_locals(regions=regions)
     r_clusters = raw_clusters(indexes)
     m_clusters = merge_clusters(clusters=r_clusters, maxs=maxs_list, window=window)
-    f_clusters = clusters_mut(clusters=m_clusters, regions=regions, mutations=mutations, mut_cutoff=cutoff)
-    s_clusters = score_clusters(clusters=f_clusters, regions=regions, mutations=mutations, mode=mode)
+    f_clusters = clusters_mut(clusters=m_clusters, regions=regions, mutations=mutations)
+    s_clusters = score_clusters(clusters=f_clusters, regions=regions)
     
     return s_clusters
 
-def score_genes(clusters, mode):
+
+# Score genes
+def score_gene(clusters, cutoff):
     """
     Given the clusters of a region, calculate a global score for it
     :param clusters: dictionary of dictionaries
-    :param mode: str, scoring
-    :return: dictionary  of keys 'score' and 'n_clusters' number of clusters for a region
+    :param cutoff: int, n cluster mutations cutoff
+    :return: int, int; number of clusters and gene score
     """
-    clusters_score = []
-    result_d = defaultdict()
+    n_clusters = []
+    score = []
 
-    for cluster, values in clusters.items():
-        # get cluster width
-        width = 0
-        if values['min_l'] != [] and values['min_r'] != []:
-            width = abs(values['min_l'][0] - values['min_r'][0])
-        elif values['min_l'] == []:
-            width = abs(values['max'][0] - values['min_r'][0])
-        elif values['min_r'] == []:
-            width = abs(values['min_l'][0] - values['max'][0])
-        clusters_score.append((values['score'], width))
+    for cluster, value in clusters.items():
+        if value['n_mutations'] >= cutoff:
+            n_clusters.append(1)
+            score.append(value['score'])
 
-    result_d['n_clusters'] = len(clusters.keys())
-    if mode == 'sum':
-        result_d['score'] = sum(i for i, j in clusters_score)
-    elif mode == 'mean':
-        result_d['score'] = sum(i for i, j in clusters_score) / len(clusters.keys())
-    elif mode == 'wmean':
-        result_d['score'] = sum(i/j for i, j in clusters_score) / len(clusters.keys())
+    gene_score = 0
+    if n_clusters:
+        gene_score = sum(score) / sum(n_clusters)
 
-    return result_d
+    return sum(n_clusters), gene_score
--- oncodcl_run.py
+++ oncodcl_run.py
@@ -1,83 +1,37 @@
-
-
-# Oncodriveclustl functions
+# Oncodriveclustl run
 import sys
-import argparse
-import re
 from concurrent.futures import ProcessPoolExecutor as Pool
+from tqdm import tqdm
 from collections import defaultdict
 
-import pandas as pd
-from tqdm import tqdm
 import oncodcl_funct as odf
+import oncodcl_funct2 as odf3
 
-parser = argparse.ArgumentParser(description="""OncodriveCLUSTL testing""")
+def main(regions_d, mutations_d, cores=4):
+    """Main function
+    :param regions_d: dictionary of dictionaries containing genomic regions
+    :param mutations_d: dictionary of lists, 'gene' : [mutations]
+    :param cores: int, number of CPUs to use
+    :return: None
+    """
+    results_d = defaultdict()
 
-parser.add_argument("-GM", "--genemut",
-                    dest = "GM",
-                    action = "store",
-                    default = 2,
-                    type = int,
-                    help = "Input number of mutations required in a gene")
+    # Analyze only genes with >= 2 mutations
+    genes = [(g, regions_d[g], mutations_d[g]) for g in regions_d.keys() if len(mutations_d[g]) >= 2]
 
-parser.add_argument("-CM", "--clustermut",
-                    dest = "CM",
-                    action = "store",
-                    default = 2,
-                    type = int,
-                    help = "Input number of mutations required in a cluster")
+    with Pool(max_workers=cores) as executor, tqdm(total=len(genes)) as pbar:
+        for result in executor.map(odf3.run_region, genes):
+            pbar.update(1)
+            # 'gene' : (length, n clusters, total mutations, score)
+            results_d[result[0]] = (result[1], result[2], result[3], result[4])
 
-parser.add_argument("-SW", "--smoothw",
-                    dest = "SW",
-                    action = "store",
-                    default = None,
-                    required = True,
-                    type = int,
-                    help = "Smoothing window")
+        print(len(results_d.keys()))
+        sys.stderr.write('Results calculated\n')
 
-parser.add_argument("-CW", "--clusterw",
-                    dest = "CW",
-                    action = "store",
-                    default = None,
-                    required = True,
-                    type = int,
-                    help = "Cluster window")
-
-parser.add_argument("-CS", "--clusterscore",
-                    dest = "clusterscore",
-                    default = "nobias",
-                    action = "store",
-                    help = "Cluster score: 'nmutations', 'genelength', 'nobias' ")
-
-parser.add_argument("-GS", "--genescore",
-                    dest = "genescore",
-                    action = "store",
-                    default = "mean",
-                    help = "Gene score: 'sum', 'mean', 'wmean'")
-
-parser.add_argument('-o', '--output',
-                    dest = "outfile",
-                    action = "store",
-                    default = None,
-                    required=True,
-                    help = "Output file prefix without extension")
-
-args = parser.parse_args()
-
-sys.stderr.write("OncodriveCLUSTL testing\n")
-sys.stderr.write("\tGene mutations threshold (GM):\t%s\n" %args.GM)
-sys.stderr.write("\tCluster mutations threshold (CM): %s\n" %args.CM)
-sys.stderr.write("\tSmoothing window (SW):\t%s\n" %args.SW)
-sys.stderr.write("\tClustering window (CW):\t%s\n" %args.CW)
-sys.stderr.write("\tCluster score mode (CS): %s\n" %args.clusterscore)
-sys.stderr.write("\tGene score mode (GS):\t%s\n" %args.genescore)
-outfile_prefix = re.sub('\..*', '', str(args.outfile)) # Remove any extension added
-outfile_file = outfile_prefix+"_"+str(args.GM)+"_"+str(args.CM)+"_"+str(args.SW)+"_"+str(args.CW)+"_"+str(args.clusterscore)+"_"+str(args.genescore)
-sys.stderr.write("\tOutput file:\t\t%s\n" %outfile_file)
 
 # Parse regions
 input_regions = '/home/carnedo/projects/oncodriveclustl/inputs/regions/02_cds.regions.gz'
-trees, regions_d, strands_d = odf.regions(input_regions)
+trees, regions_d = odf.regions(input_regions)
 sys.stderr.write('Regions parsed\n')
 
 # Read mutations, intersect with regions
@@ -85,66 +39,5 @@ input_mutations = '/home/carnedo/projects/oncodriveclustl/inputs/mutations/panca
 mutations_d = odf.read_mutations(input_mutations, trees)
 sys.stderr.write('Mutations read\n')
 
-
-# Smooth
-total_regions = defaultdict(dict)
-for gene in regions_d.keys():
-    # Analyze only genes with mutations >= threshold
-    if len(mutations_d[gene]) >= args.GM:
-        region_info = odf.smoothing(symbol=gene, regions=regions_d, mutations=mutations_d[gene], window=args.SW)
-        # dictionary with regions of all genes
-        total_regions[region_info['symbol']] = region_info
-sys.stderr.write('Smoothing finished. Total genes for clustering: %s\n' %len(total_regions.keys()))
-
-# Clusters
-total_clusters = defaultdict(dict)
-for gene in total_regions.keys():
-    clusters = odf.clustering(regions=total_regions[gene], mutations=mutations_d[gene], window=args.CW, cutoff=args.CM, mode=args.clusterscore)
-    if len(clusters.keys()) != 0:
-        total_clusters[gene] = clusters
-sys.stderr.write('Clustering finished. Total genes analyzed: %s\n' %len(total_clusters.keys()))
-
-# Score genes
-genes_results = defaultdict()
-for gene, value in total_clusters.items():
-    print(gene, value)
-    result = odf.score_genes(clusters=value, mode=args.genescore)
-    genes_results[gene] = result
-
-# Output
-CGC = set([line.split('\t')[0] for line in open('/home/carnedo/projects/oncodriveclustl/inputs/CGC/CGCMay17_cancer_types_TCGA.tsv', 'r')])
-out = '/home/carnedo/projects/oncodriveclustl/outputs/20170731'
-
-genes_table = out + outfile_file
-
-with open(genes_table, 'w') as fd:
-    fd.write('Gene\tLength\tClusters\tMutations\tScore\n')
-    for gene, result in genes_results.items():
-        fd.write("%s\t%s\t%s\t%s\t%s\n" % (gene, len(total_regions[gene]['genomic']), result['n_clusters'], len(mutations_d[gene]), result['score']))
-
-df = pd.read_csv(genes_table, sep='\t', header=0)
-df['CGC'] = df['Gene'].map(lambda x: x in CGC)
-df.sort_values(by=['Score', 'CGC'], ascending=[False, False], inplace=True)
-df.to_csv(path_or_buf=genes_table, sep='\t', na_rep='', index=False)
-
-
-def run(gene):
-    smooting()
-    clustering()
-    score()
-    write_output()
-    return gene, result
-
-
-def main(cores=4, genes):
-    """Main function
-    :param cores: int, number of CPUs to use
-    :param genes: lis, list of genes to analize
-    :return: None
-    """
-    results = dict()
-    with Pool(max_workers=cores) as executor, tqdm(total=len(genes)) as pbar:
-        for geneid, result in executor.map(run, genes):
-            pbar.update(1)
-            results[geneid] = result
-            # save(results, output_file)
+if __name__ == '__main__':
+    main(regions_d, mutations_d)
